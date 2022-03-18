2ba4b912e0ca63bae62fc0eecc06401d4ead11de
fuunylmz <2538212014@qq.com>
2022-03-18 14:56:42 +0800

    reverse string

--- 循环&关系表达式/forstring.cpp
+++ 循环&关系表达式/forstring.cpp
@@ -1,4 +1,16 @@
 //
 // Created by fuuny_lmz on 2022/3/18.
 //
+#include "../header/base.h"
 
+int main(){
+    using namespace std;
+    //反转字符串
+    string input;
+    cout << "输入一个字符串：";
+    cin >> input;
+    for(int i = input.size() - 1;i >= 0;i--){
+        cout << input[i];
+    }
+    return 0;
+}
