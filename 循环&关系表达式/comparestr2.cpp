e15b979108552952395ba2460e4a6da9b697c7fd
fuunylmz <2538212014@qq.com>
2022-03-21 14:39:40 +0800

    update

--- 循环&关系表达式/comparestr2.cpp
+++ 循环&关系表达式/comparestr2.cpp
@@ -2,7 +2,7 @@
 // Created by fuuny_lmz on 2022/3/21.
 //
 #include "../header/base.h"
-int main(){
+int main12(){
     using namespace std;
     string word = "?ord";
     for(char ch = 'a';word != "word";ch++){
